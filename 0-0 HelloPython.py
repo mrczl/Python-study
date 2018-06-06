01b70c10f65e6d01567ccaaeb8a0f3bc65d51791
mrczl <1415782655@qq.com>
2018-06-06 19:19:39 +0800

    Hello Python
    Hello Python
    print()

---
+++ 0-0 HelloPython.py
@@ -0,0 +1,11 @@
+# 0-0 hello Python
+print ("Hello Python!!!")
+print ("Hello Python!!!")
+print ("Hello Python!!!")
+print ("****************")
+print ("Python is powerful... and fast;\
+plays well with others;\
+runs everywhere;\
+is friendly & easy to learn;\
+is Open.")
+print ("****************")
