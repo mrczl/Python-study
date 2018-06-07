c39687792e2e7f4ba7aa9a0c20c4f2dcea58b89d
mrczl <1415782655@qq.com>
2018-06-07 22:36:14 +0800

    Hello Python
    1-2 Numbers and Math

---
+++ 1-2 Numbers and Math.py
@@ -0,0 +1,10 @@
+# 1-2 Numbers and Math
+# Some math symbols names:
+print("There are Some math symbols names: ")
+print("-> + plus\n-> - minus\n-> / slash\n-> * asterisk\n-> % percent\n-> < less-than\n-> > greater-than\n-> <= less-than-equal\n-> >= greater-than-equal")
+print("")
+print("1314+520=",1314+520)
+print("2/7=",2/7)
+print("-2>7",2>7)
+print("9>100",9>100)
+print("50<60",50<60)
