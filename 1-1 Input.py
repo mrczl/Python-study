6ba9b704c6ebc5a97ad28eaaaee97e994096dda9
mrczl <1415782655@qq.com>
2018-06-07 13:11:21 +0800

    Revert "1-1 Input"
    This reverts commit aa9e897ebad8e5a5eb33c5fee789d66471396da5.

---
+++ 1-1 Input.py
@@ -0,0 +1,9 @@
+# 1-1 input
+# input(prompt=None, /)
+#   Read a string from standard input.
+
+print('----------1-1 Input---------')
+name = input("Please enter your name:->")
+age = input ("Please enter your age:->")
+print("Hi!,My name is "+name+",and I'm "+age+",Nice to meet you! \
+Welcome to join our club.")
