6731c43a654050737cdd3810c88bb843ca4cd6c9
mrczl <1415782655@qq.com>
2018-06-08 13:12:41 +0800

    1-3 List
    add 1-3 List

---
+++ 1-3 List 1.py
@@ -0,0 +1,21 @@
+# 1-3 List 列表
+list1 = ['Baidu','Google',1314,3.1415926];
+list2 = [1,2,3,4,5,6];
+
+print("list1:",list1)
+print("list2:",list2)
+print("list1[1]:",list1[1])
+print("list2[2:5]",list2[2:5])
+# 更新列表元素
+print("------更新列表元素------")
+list4 = ['Baidu','Google',28257,1314,3.1415926];
+print("list4.append(292831)")
+list4.append(292831)
+print("list4:",list4)
+# 删除列表元素
+print("------删除列表元素------")
+list5 = ['Baidu','Google',28257,1314,3.1415926];
+print("list5 = ['Baidu','Google',28257,1314,3.1415926];")
+print("del list5[3]")
+del list5[3]
+print("list5:",list5)
