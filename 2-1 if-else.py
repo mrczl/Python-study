cee1d697835f863df060379b9124983a09f7dd87
mrczl <1415782655@qq.com>
2018-07-03 18:18:16 +0800

    if-else
    add if-else statement

---
+++ 2-1 if-else.py
@@ -0,0 +1,7 @@
+# 2-1 if-else
+a = 6
+b = 12
+if (a > b):
+    print ("a is greater than b")
+else:
+    print ("a is less than b")
---
+++ 2-2 if-else2.py
@@ -0,0 +1,11 @@
+num = int(input("Please enter a two-digit number >"))
+if (num < 0):
+    print("num is minus")
+elif (num == 0):
+    print("num is O")
+elif (50 < num <100):
+    print("num is greater than 50")
+elif (0 < num <51):
+    print("num is greater than O")
+else:
+    print("number is must two-digit number")
