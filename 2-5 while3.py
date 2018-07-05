0e16ccea09e06631fa7282c8979bb3be35411b31
mrczl <1415782655@qq.com>
2018-07-05 13:33:50 +0800

    while
    amend 2-5 while3

--- 2-5 while3.py
+++ 2-5 while3.py
@@ -3,7 +3,7 @@ print("-" * 50)
 while i < 11:
     n = 1
     while n <= 10:
-        print("%4d" % (i * n), end=' ')
+        print("%4d" % (i * n), end=" ")
         n += 1
     print()
     i += 1
