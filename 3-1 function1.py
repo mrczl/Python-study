3d3e5cad5002a61d1a464872b827a7a6bcf2f403
mrczl <1415782655@qq.com>
2018-07-06 17:48:21 +0800

    function
    add 3-1 function1.py

---
+++ 3-1 function1.py
@@ -0,0 +1,10 @@
+# 3-1 function
+"""
+ Deﬁning a function
+ We use def keyword to deﬁne a function. General syntax is like
+     def functionname(params):
+        statement1
+        statement2
+"""
+def helloworld(a,b):
+    print("sum =",a+b)
