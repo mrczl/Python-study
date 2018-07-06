15c67d54254973248d3f4eec2aeb63aea974ca21
mrczl <1415782655@qq.com>
2018-07-06 11:33:02 +0800

    add file
    if - else and  while loop

---
+++ 2-3 while1.py
@@ -0,0 +1,5 @@
+# 2-3 while loop
+n = 0
+while n < 11:    # condition:
+    print(n)     # statement
+    n += 1
---
+++ 2-6 while6.py
@@ -0,0 +1,4 @@
+n = int(input("Please enter a number:"))
+while n > 0:
+    print("*" * n)
+    n -= 1
